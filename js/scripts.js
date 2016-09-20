590a1840cd360c9e6bdbf214004be3d41e2de9cc
imarrsh <marshallthomp@gmail.com>
2016-09-19 21:03:31 -0400

    added scripts js and new images

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+(function(){
+  function hello(){
+    console.log("hello world");
+  }
+
+  var carousel = document.querySelectorAll('.carousel img');
+
+
+  function getFlickrFeed(){
+    // idk what im doing
+  }
+
+  //window.addEventListener('load', hello);
+  window.addEventListener('load', getFlickrFeed);
+
+}());
